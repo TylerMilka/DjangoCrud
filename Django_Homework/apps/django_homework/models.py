58b6541641622596d20d88f63d4f1d8e190a9e64
Tyler <tgmilka@gmail.com>
2017-03-12 16:03:20 -0500

    models made

--- Django_Homework/apps/django_homework/models.py
+++ Django_Homework/apps/django_homework/models.py
@@ -1,3 +1,12 @@
+
 from django.db import models
 
 # Create your models here.
+
+class CodeSnippets(models.Model):
+    name = models.CharField(max_length = 45)
+    language = models.CharField(max_length = 45)
+    code = models.CharField(max_length = 300)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    
